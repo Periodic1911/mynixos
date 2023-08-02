2fa00d753520e75f6c17fa0a98a0749a9f7f686e
Anne Stijns <anstijns@gmail.com>
2023-08-02 17:46:07 +0200

    dark color scheme

--- home-manager/dark.nix
+++ home-manager/dark.nix
@@ -1,9 +1,15 @@
-{ config, pkgs, ... }:
+{ config, pkgs, nix-colors, ... }:
 
 {
   home.stateVersion = "22.11"; # Please read the comment before changing.
 
-  # Dark theme
+  imports = [
+    nix-colors.homeManagerModules.default
+  ];
+
+  colorScheme = nix-colors.lib.schemeFromYAML "shades-of-purple" (builtins.readFile ./shades-of-purple.yaml);
+
+  #Make GTK and QT use dark mode
   gtk = {
     enable = true;
     theme.package = pkgs.gnome.gnome-themes-extra;
@@ -16,6 +22,17 @@
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
+    };
+  };
+
+  programs = {
+    kitty = {
+      enable = true;
+      settings = {
+        foreground = "#${config.colorScheme.colors.base05}";
+        background = "#${config.colorScheme.colors.base00}";
+        # ...
+      };
     };
   };
 }
--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -8,9 +8,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, nix-colors, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -27,10 +28,12 @@
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
+        extraSpecialArgs = { inherit nix-colors; };
       };
       homeConfigurations.anne-dark = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./home.nix ./dark.nix ];
+        extraSpecialArgs = { inherit nix-colors; };
       };
     };
 }
