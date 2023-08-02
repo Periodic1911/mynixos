2fea80b41ca2d62ed90fd17fa0e09aad68718810
Anne Stijns <anstijns@gmail.com>
2023-08-02 17:54:06 +0200

    light color scheme

--- home-manager/light.nix
+++ home-manager/light.nix
@@ -1,4 +1,22 @@
-{ config, pkgs, ... }:
+{ config, pkgs, nix-colors, ... }:
 
 {
+  home.stateVersion = "22.11"; # Please read the comment before changing.
+
+  imports = [
+    nix-colors.homeManagerModules.default
+  ];
+
+  colorScheme = nix-colors.colorSchemes.atelier-dune-light;
+
+  programs = {
+    kitty = {
+      enable = true;
+      settings = {
+        foreground = "#${config.colorScheme.colors.base05}";
+        background = "#${config.colorScheme.colors.base00}";
+        # ...
+      };
+    };
+  };
 }
